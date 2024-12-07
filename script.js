ee24c20f818e91c1c10ee2ec9f641570612bbd9d
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2024-12-07 01:56:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -113,10 +113,10 @@ document.addEventListener("DOMContentLoaded", () => {
           explanation: "A test cross is performed to determine the genotype of an individual with a dominant phenotype by crossing it with a homozygous recessive individual."
         }, 
         // Add more Genetics questions here...
-      ], 
+      ]
     }, 
       
-  ZOO101: {
+  "ZOO101": {
     title: Advanced Technical Questions on Phylum Chordata and Vertebrata,
     questions: [
       {
