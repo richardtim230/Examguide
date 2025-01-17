c587979994869db6d8065e43fa251a6064d34b45
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-01-17 09:30:42 +0100

    Update mock-script.js

--- mock-script.js
+++ mock-script.js
@@ -1300,7 +1300,7 @@ resultsContent.innerHTML = questions.map((q, i) => {
 
 async function generatePDF() {
   const { jsPDF } = window.jspdf; // Import jsPDF
-  const logo = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAA...'; // Base64 encoded logo image
+  const logo = 'logo.png'; // Base64 encoded logo image
   const doc = new jsPDF({
     orientation: "portrait",
     unit: "px",
