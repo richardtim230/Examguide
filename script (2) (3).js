4a66157c77d36af7075713e28799c706f453c51c
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-02-04 02:24:52 +0100

    Update script (2) (3).js

--- script (2) (3).js
+++ script (2) (3).js
@@ -4672,7 +4672,7 @@ document.addEventListener("DOMContentLoaded", () => {
         title: "Biometry/Biostatistics",
         questions: [
           {
-  text: "Which of the following is an example of an infinite set? <br><img src='row-bookcases_23-2147679267.webp' alt='Infinite Set Image' class='question-image'>",
+  text: "Which of the following is an example of an infinite set? <br><img src='row-bookcases_23-2147679267.webp' alt='Infinite Set Image' width="500" class='question-image'>",
   options: [
     "{x | x is a natural number between 1 and 10}",
     "{x | x is an integer}",
