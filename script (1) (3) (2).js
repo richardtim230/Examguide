d4aedd84fe4bb20d25345bd4bc2f169ec9460deb
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-02-25 19:44:57 +0100

    Update script (1) (3) (2).js

--- script (1) (3) (2).js
+++ script (1) (3) (2).js
@@ -1,3 +1,50 @@
+function openDatabase() {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open('myAppDatabase', 1);
+    
+    request.onupgradeneeded = (event) => {
+      const db = event.target.result;
+      db.createObjectStore('myStore', { keyPath: 'id' });
+    };
+    
+    request.onsuccess = (event) => {
+      resolve(event.target.result);
+    };
+
+    request.onerror = (event) => {
+      reject(event.target.error);
+    };
+  });
+}
+
+function saveData(data) {
+  openDatabase().then(db => {
+    const transaction = db.transaction('myStore', 'readwrite');
+    const store = transaction.objectStore('myStore');
+    store.put(data);
+  }).catch(error => {
+    console.error('Error saving data', error);
+  });
+}
+
+function getData(id) {
+  return new Promise((resolve, reject) => {
+    openDatabase().then(db => {
+      const transaction = db.transaction('myStore', 'readonly');
+      const store = transaction.objectStore('myStore');
+      const request = store.get(id);
+
+      request.onsuccess = (event) => {
+        resolve(event.target.result);
+      };
+
+      request.onerror = (event) => {
+        reject(event.target.error);
+      };
+    });
+  });
+}
+
 const ws = new WebSocket('ws://localhost:8080');
 
 ws.onopen = () => {
