1864ca26e96f20dd51e122a84255d8ed4890ebdf
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-03-17 05:02:35 +0100

    Update gen-script.js

--- gen-script.js
+++ gen-script.js
@@ -1,3 +1,12 @@
+function toggleMenu() {
+            var menu = document.getElementById("menu");
+            if (menu.style.display === "flex") {
+                menu.style.display = "none";
+            } else {
+                menu.style.display = "flex";
+            }
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     let slider = document.querySelector(".slider");
     let index = 0;
