e4f250aa69e0af7a4e4412201465bf7b5de7b853
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-03-28 10:43:04 +0100

    Update tp.js

--- tp.js
+++ tp.js
@@ -134,8 +134,8 @@ function redeemCredits() {
     "2280281834": 200,  // Admin set PIN 3
     "1333744582": 200,    // Admin set PIN 3
     "6472415768": 20000,   // Admin set PIN 3
-    "1899501691": 200,     // Admin set PIN 3
-        "2535410145": 200,     // Admin set PIN 3
+    "5310613899": 300,     // Admin set PIN 3
+        "9479187153": 300,     // Admin set PIN 3
         "476304902": 200,     // Admin set PIN 3
         "476304902": 200,     // Admin set PIN 3
         "476304902": 200,     // Admin set PIN 3
