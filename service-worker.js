26c4acf1daf8ebba4aab1dbad5356dd1744e6980
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-03-29 20:46:22 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -23,3 +23,19 @@ self.addEventListener('push', event => {
         self.registration.showNotification(data.title, options)
     );
 });
+
+self.addEventListener('notificationclick', event => {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: 'window' }).then(clientList => {
+            for (const client of clientList) {
+                if (client.url === '/' && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            if (clients.openWindow) {
+                return clients.openWindow('/');
+            }
+        })
+    );
+});
