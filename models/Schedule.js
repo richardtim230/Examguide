9c45e34a82930625d8a0121f77808cf27cfb5d40
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-06-19 19:34:54 +0100

    Update Schedule.js

--- models/Schedule.js
+++ models/Schedule.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
+
 const ScheduleSchema = new mongoose.Schema({
-  examSet: { type: String, required: true },
+  // Reference to QuestionSet for population in API
+  examSet: { type: mongoose.Schema.Types.ObjectId, ref: "QuestionSet", required: true },
   faculty: { type: String, required: true },
   department: { type: String, required: true },
   start: { type: Date, required: true },
   end: { type: Date, required: true },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
 }, { timestamps: true });
+
 export default mongoose.model("Schedule", ScheduleSchema);
