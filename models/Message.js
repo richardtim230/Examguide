9b63b45e07a1f644f20afa3aee79f6a2e67e22d2
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-06-19 21:02:11 +0100

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 const MessageSchema = new mongoose.Schema({
-  from: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  to: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  from: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
+  to: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
   text: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
-  read: { type: Boolean, default: false }
+  read: { type: Boolean, default: false },
+  type: { type: String, enum: ["user", "system", "group"], default: "user" }
 });
 export default mongoose.model("Message", MessageSchema);
