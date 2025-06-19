8a2f1cd3edd77867208a8eea420b7d01bf38b907
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-06-19 02:32:24 +0100

    Create Message.js

---
+++ models/Message.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+const MessageSchema = new mongoose.Schema({
+  from: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  to: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  text: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  read: { type: Boolean, default: false }
+});
+export default mongoose.model("Message", MessageSchema);
