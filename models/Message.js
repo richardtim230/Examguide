b6add81177c4dff4f125059fc50f455b4ad40eae
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-06-20 14:38:49 +0100

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,10 +1,19 @@
 import mongoose from "mongoose";
+const FileSchema = new mongoose.Schema({
+  originalname: String,
+  mimetype: String,
+  filename: String,
+  size: Number,
+  url: String
+}, { _id: false });
+
 const MessageSchema = new mongoose.Schema({
   from: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
   to: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
-  text: { type: String, required: true },
+  text: { type: String, required: false }, // now allow empty for pure file messages
   createdAt: { type: Date, default: Date.now },
   read: { type: Boolean, default: false },
-  type: { type: String, enum: ["user", "system", "group"], default: "user" }
+  type: { type: String, enum: ["user", "system", "group"], default: "user" },
+  file: { type: FileSchema, required: false } // Add file support!
 });
 export default mongoose.model("Message", MessageSchema);
