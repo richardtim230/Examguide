d3e5ba488b3512f7ceb44c687db994235ad8a69f
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-06-20 12:57:52 +0100

    Update db-script.js

--- db-script.js
+++ db-script.js
@@ -3,12 +3,7 @@
 import sqlite3 from 'sqlite3';
 const db = new sqlite3.Database('mydb.sqlite');
 // Open a database connection
-let db = new sqlite3.Database('./database.db', (err) => {
-    if (err) {
-        console.error(err.message);
-    }
-    console.log('Connected to the SQLite database.');
-});
+
 
 // Create a table
 db.run(`CREATE TABLE IF NOT EXISTS users (
