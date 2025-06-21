6058787949f24532bfb63360084e5c95cea21de5
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-06-21 03:41:14 +0100

    Update Notification.js

--- models/Notification.js
+++ models/Notification.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+
 const NotificationSchema = new mongoose.Schema({
   title: { type: String, required: true },
   message: { type: String, required: true },
+  imageUrl: { type: String }, // New field for image URL
   sentBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  sentTo: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  createdAt: { type: Date, default: Date.now }
-});
+  sentTo: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+}, { timestamps: true });
+
 export default mongoose.model("Notification", NotificationSchema);
