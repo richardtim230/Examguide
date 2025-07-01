e91c8b20031c0cd4dc79bb80b754f4756ae7aeb1
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-07-01 23:38:07 +0100

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 const AdminSchema = new mongoose.Schema({
   fullname: { type: String, required: true },
-  email: { type: String, required: true, unique: true, lowercase: true },
-  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true, lowercase: true, index: true },
+  username: { type: String, required: true, unique: true, index: true },
   password: { type: String, required: true }
 }, { timestamps: true });
+
 export default mongoose.model("Admin", AdminSchema);
