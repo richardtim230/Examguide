82a2a1d61e9b638bc529b824a6077c390e24ecb2
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-07-02 09:15:02 +0100

    Update Form.js

--- models/Form.js
+++ models/Form.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
-const FormSchema = new mongoose.Schema({
+
+const formSchema = new mongoose.Schema({
   name: { type: String, required: true },
   type: { type: String, required: true },
-  code: { type: String, required: true, unique: true, uppercase: true },
-  desc: { type: String },
+  code: { type: String, required: true, unique: true },
+  desc: String,
+  admin: { type: mongoose.Schema.Types.ObjectId, ref: "Admin", required: true }
 }, { timestamps: true });
-export default mongoose.model("Form", FormSchema);
+
+export default mongoose.model("Form", formSchema);
