3cc70c3019231f833ddf208e2fbdacfd8e897dcc
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-07-02 21:59:51 +0100

    Update Form.js

--- models/Form.js
+++ models/Form.js
@@ -2,15 +2,11 @@ import mongoose from "mongoose";
 
 const FormSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  code: { type: String, required: true, unique: true }, // e.g. for URL code
-  desc: { type: String },
-  admin: { type: mongoose.Schema.Types.ObjectId, ref: "Admin" },
-  fields: [{ 
-    label: String, 
-    type: String, // e.g. "text", "email", "number", "file", etc.
-    options: [String], // for select/radio types
-    required: Boolean
-  }]
+  type: { type: String, required: true },
+  code: { type: String, required: true, unique: true },
+  desc: String,
+  admin: { type: mongoose.Schema.Types.ObjectId, ref: "Admin", required: true }
 }, { timestamps: true });
+
 
 export default mongoose.models.Form || mongoose.model("Form", FormSchema);
