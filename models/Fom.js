1116bbf29ec0d583b1ce2a6fad1c824a88394c60
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-07-02 21:37:39 +0100

    Update Fom.js

--- models/Fom.js
+++ models/Fom.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 
-const ResponseSchema = new mongoose.Schema({
-  form: { type: mongoose.Schema.Types.ObjectId, ref: "Form", required: true },
-  data: { type: Object, required: true }, // Submitted fields and answers
-  date: { type: Date, default: Date.now },
-  createdAt: { type: Date, default: Date.now }
-});
+const FormSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  code: { type: String, required: true, unique: true }, // e.g. for URL code
+  desc: { type: String },
+  admin: { type: mongoose.Schema.Types.ObjectId, ref: "Admin" },
+  fields: [{ 
+    label: String, 
+    type: String, // e.g. "text", "email", "number", "file", etc.
+    options: [String], // for select/radio types
+    required: Boolean
+  }]
+}, { timestamps: true });
 
-export default mongoose.model("Response", ResponseSchema);
+export default mongoose.models.Form || mongoose.model("Form", FormSchema);
