58142e7b29fd1fe2ef3e60e07a01fef828180698
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-07-02 21:36:28 +0100

    Update Response.js

--- models/Response.js
+++ models/Response.js
@@ -1,14 +1,9 @@
 import mongoose from "mongoose";
 
 const responseSchema = new mongoose.Schema({
-  form: { type: mongoose.Schema.Types.ObjectId, ref: "Form", required: true },
   formId: { type: mongoose.Schema.Types.ObjectId, ref: "Form", required: true },
   data: { type: Object, required: true },
-  date: { type: Date, default: Date.now },
-  createdAt: { type: Date, default: Date.now }
+  date: { type: Date, default: Date.now }
 }, { timestamps: true });
 
 export default mongoose.model("Response", responseSchema);
-
-
-
