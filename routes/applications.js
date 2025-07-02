3386a4fe66c79ab393b72bcb0cc7501a4b016ce1
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-07-02 21:21:11 +0100

    Update applications.js

--- routes/applications.js
+++ routes/applications.js
@@ -2,7 +2,7 @@ import express from "express";
 import multer from "multer";
 import path from "path";
 import Application from "../models/Application.js";
-import Form from "../models/Form.js";
+import Form from "../models/Response.js";
 
 const router = express.Router();
 
