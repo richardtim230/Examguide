c7a36189ec69b551bd0a709c65530ec2e632422d
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-07-26 13:23:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ import usersRoutes from "./routes/users.js";
 import formsRoutes from "./routes/forms.js"; // <-- add this line
 import registrationsRoutes from "./routes/registrations.js";
 import applicationsRoutes from "./routes/applications.js";
-import bloggerRoutes from './routes/blogger.routes.ts';
+
 
 dotenv.config();
 
@@ -297,7 +297,7 @@ app.delete("/api/progress", authenticate, async (req, res) => {
 app.use("/api/superadmin", superadminRoutes);
 
 // --- Main Features ---
-app.use('/api', bloggerRoutes);
+
 app.use("/api/questionsets", questionSetRoutes);
 app.use("/api/results", resultsRoutes);
 app.use("/api/schedules", scheduleRoutes);
