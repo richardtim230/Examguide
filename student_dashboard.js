89a93475afab2ba3e754edb01d0ee5e6478b484d
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-08-07 07:50:01 +0100

    Update student_dashboard.js

--- student_dashboard.js
+++ student_dashboard.js
@@ -1994,9 +1994,10 @@ async function initDashboard() {
   await fetchLeaderboard();
   renderLeaderboard();
   await fetchAnnouncements();
+  await fetchInbox();
   hidePreloaderSpinner();
   await fetchAvailableTests();
-  await fetchInbox();
+  
   
 }
 
