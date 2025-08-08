a80cd7038fd086d99dc6e2019f85a267f40ac236
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-08-08 10:09:14 +0100

    Update UserAnswer.js

--- models/UserAnswer.js
+++ models/UserAnswer.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const UserAnswerSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
@@ -17,4 +17,5 @@ const UserAnswerSchema = new mongoose.Schema({
   submittedAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('UserAnswer', UserAnswerSchema);
+export default mongoose.model('UserAnswer', UserAnswerSchema);
+ 
