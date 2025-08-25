2152ffa221ae76d17336a146b3dd061cb67ef64f
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-08-25 06:46:36 +0100

    Update Question.js

--- models/Question.js
+++ models/Question.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const QuestionSchema = new mongoose.Schema({
   subject: { type: String, required: true },
   year: { type: String, required: true },
-  difficulty: { type: String, enum: ['Easy', 'Medium', 'Hard'], required: true },
+  courseCode: { type: String, required: true }, // Changed from 'difficulty'
   text: { type: String, required: true }, // Question text
   image: { type: String }, // Optional image URL or file reference
   options: [{ type: String, required: true }], // Choices
@@ -10,10 +10,9 @@ const QuestionSchema = new mongoose.Schema({
   explanation: { type: String, default: "" }, // Optional explanation
   topic: { type: String, default: "" },
   tags: [{ type: String }],
-  createdBy: { type: mongoose.Schema.Types.ObjectId},
+  createdBy: { type: mongoose.Schema.Types.ObjectId },
   createdAt: { type: Date, default: Date.now }
   // You can add a field for file type if supporting different media types
 });
-
 
 export default mongoose.model("Question", QuestionSchema);
