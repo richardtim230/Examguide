eeae05067a8a07e508ae9885592b634898ce0fa4
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-09-30 20:48:21 +0100

    Create myPosts.js

---
+++ routes/myPosts.js
@@ -0,0 +1,20 @@
+// routes/myPosts.js (new route file for personal posts)
+import express from "express";
+import Post from "../models/Post.js";
+import { authenticate } from "../middleware/authenticate.js";
+
+const router = express.Router();
+
+router.get("/myposts", authenticate, async (req, res) => {
+  try {
+    const myPosts = await Post.find({ author: req.user.id }).sort({ date: -1 });
+    res.json(myPosts.map(post => ({
+      ...post.toObject(),
+      _id: post._id?.toString() ?? ""
+    })));
+  } catch (err) {
+    res.status(500).json({ error: "Could not fetch posts." });
+  }
+});
+
+export default router;
