f9ab5a363d2418e76398c843aede114d82173405
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-10-01 04:51:32 +0100

    Update migratePosts.js

--- scripts/migratePosts.js
+++ scripts/migratePosts.js
@@ -5,21 +5,22 @@ dotenv.config();
 import BloggerDashboard from "../models/BloggerDashboard.js";
 import Post from "../models/Post.js";
 
-// Use your .env MONGODB_URI
 const MONGO_URI = process.env.MONGODB_URI;
 
 async function migratePosts() {
   await mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
   const dashboards = await BloggerDashboard.find({});
   let migrated = 0;
   let errors = 0;
 
   for (const dash of dashboards) {
     for (const post of dash.posts || []) {
+      // Check if already migrated
+      const exists = await Post.findOne({ _id: post._id });
+      if (exists) continue;
       const newPost = new Post({
         ...post.toObject(),
-        author: dash.user, // Attach the dashboard owner as author
+        author: dash.user,
       });
       try {
         await newPost.save();
