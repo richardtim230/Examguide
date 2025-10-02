153a83e901d28df580365ad243e3709d376791d4
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2025-10-02 01:24:32 +0100

    Update Result.js

--- models/Result.js
+++ models/Result.js
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
 const ResultSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  examSet: { type: mongoose.Schema.Types.ObjectId, ref: "QuestionSet", required: true },
+  examSet: { type: mongoose.Schema.Types.ObjectId, ref: "QuestionSet" }, // remove required:true
+  examSetTitle: { type: String },    // Add this for practice mode
+  subject: { type: String },
+  year: { type: String },
+  courseCode: { type: String },
+  type: { type: String },            // "practice", "mock", "exam", etc.
   answers: { type: Map, of: String, default: {} },
   score: { type: Number, required: true },
   timeTaken: { type: Number, required: true },
