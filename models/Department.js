35c3b01eb56045b37191effdf875e3b798b8f95e
SamsiKay2024 <samuelkay619@gmail.com>
2025-10-16 02:32:49 +0100

    Refactor DepartmentSchema definition for brevity

--- models/Department.js
+++ models/Department.js
@@ -1,11 +1,8 @@
 import mongoose from "mongoose";
 
 const DepartmentSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
+  name: { type: String, required: true, trim: true },
+  faculty: { type: mongoose.Schema.Types.ObjectId, ref: "Faculty", required: true }
 }, { timestamps: true });
 
 // Prevent OverwriteModelError in dev/hot-reload/multiple imports
