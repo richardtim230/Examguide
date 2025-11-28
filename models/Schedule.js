18f609341f8beaebfcaaf66ec5375a7adb1717e8
richard230 <93567485+richard230@users.noreply.github.com>
2025-11-28 05:15:12 +0100

    Update faculty field to faculties array in Schedule schema

--- models/Schedule.js
+++ models/Schedule.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const ScheduleSchema = new mongoose.Schema({
   examSet: { type: mongoose.Schema.Types.ObjectId, ref: "QuestionSet", required: true },
-  faculty: { type: String, required: true },
-  departments: [{ type: String, required: true }], // Array of department IDs or names
-  levels: [{ type: String, required: true }],      // <-- NEW FIELD, array of levels (e.g., ["200","300"])
+  faculties: [{ type: String, required: true }],        // <-- updated to array
+  departments: [{ type: String, required: true }], 
+  levels: [{ type: String, required: true }],      
   start: { type: Date, required: true },
   end: { type: Date, required: true },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
