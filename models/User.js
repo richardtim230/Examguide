3033b323e999c667f1af80ef60ee0a9fed4134e3
Joan230647 <joanokosun0@gmail.com>
2026-01-25 13:21:09 +0100

    Implement tutor ID validation in User model
    Add validation for tutor ID in request parameters.

--- models/User.js
+++ models/User.js
@@ -2,7 +2,9 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import crypto from "crypto";
-
+if (!mongoose.isValidObjectId(req.params.id)) {
+  return res.status(400).json({ message: "Invalid tutor id" });
+}
 const { Schema, model } = mongoose;
 
 const socialSchema = new Schema({
