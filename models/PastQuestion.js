4e3eae1dd6078b00890b6d1cc335976ab751b292
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2026-02-01 11:07:56 +0100

    Create PastQuestion.js

---
+++ models/PastQuestion.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const PastQuestionSchema = new mongoose.Schema({
+  course: { type: String, required: true }, // e.g. "PHY101"
+  title: { type: String, required: true },
+  year: { type: Number, required: true },
+  type: { type: String, enum: ["midterm", "final", "quiz"], required: true },
+  description: String,
+ fileUrl: { type: [String], required: true }, // Now always array!
+  mimetype: [String], 
+  uploadedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model("PastQuestion", PastQuestionSchema);
