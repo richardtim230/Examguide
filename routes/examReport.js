5eb0db685eac6ad41f5a9bd2abe19294d6f018d8
JoeMarvis2024 <joemarvis2024@gmail.com>
2026-06-12 15:23:24 +0100

    Update examReport.js

--- routes/examReport.js
+++ routes/examReport.js
@@ -2,7 +2,7 @@
 
 import express from "express";
 import ExamReport from "../models/ExamReport.js";
-import { authenticateToken } from "../middleware/authMiddleware.js";
+import { authenticateToken } from "../middleware/authenticate.js";
 
 const router = express.Router();
 
